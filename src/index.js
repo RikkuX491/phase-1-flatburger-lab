a12c1516e88a55ac8af1663f9df8b64359098386
Ricardo Guerra <RikkuG491@gmail.com>
2025-01-13 15:53:49 -0500

    Completed deliverables

--- src/index.js
+++ src/index.js
@@ -2,20 +2,48 @@
 
 // Callbacks
 const displayBurgerDetails = (burger) => {
-    // Add code
+    const detailImageElement = document.getElementById('image')
+    detailImageElement.src = burger.image
+    const nameElement = document.getElementById('name')
+    nameElement.textContent = burger.name
+    const numberInCart = document.getElementById('number-in-cart-count')
+    numberInCart.textContent = burger.number_in_cart
 };
 
 const addToCart = () => {
-    // Add code
+    const addToCartForm = document.getElementById('add-to-cart-form')
+    addToCartForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const numberToAddInputElement = document.getElementById('number-to-add')
+        const numberInCart = document.getElementById('number-in-cart-count')
+        
+        const sum = parseInt(numberToAddInputElement.value) + parseInt(numberInCart.textContent)
+        numberInCart.textContent = sum
+    })
 }
 
 const addBurgerNamesToMenu = () => {
-    // Add code
+    fetch('http://localhost:3000/burgers')
+    .then(response => response.json())
+    .then(burgers => {
+        burgers.forEach(burger => {
+            const span = document.createElement('span')
+            span.textContent = burger.name
+            const restaurantMenu = document.querySelector('#restaurant-menu')
+            restaurantMenu.appendChild(span)
+
+            span.addEventListener('click', () => {
+                displayBurgerDetails(burger)
+            })
+        })
+    })
 };
 
 const main = () => {
-    // Invoke addBurgerNamesToMenu here
-    // Invoke addToCart here
+    document.addEventListener('DOMContentLoaded', () => {
+        addBurgerNamesToMenu()
+        addToCart()
+    })
 }
 
 main()
